c9fa61c21561c750379332c59951be76b9875033
Thamadhi Pathirana <thamadhipathirana@gmail.com>
2026-07-18 09:16:29 +0530

    Update cust_dashboard.php

--- view/cust_dashboard.php
+++ view/cust_dashboard.php
@@ -534,3 +534,4 @@ if (isset($_GET['success'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+<!--this is the edited final code of customer dashboard-->
